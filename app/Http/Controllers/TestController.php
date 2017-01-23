e943b262e55690a0b664893a0754f94b3b5c5dd0
vladimir1727 <dvn125@gmail.com>
2017-01-23 19:44:41 +0200

    start

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace itstep\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index($id){
+    	return $id;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+/*Route::get('users/{id}', function ($id) {
+	//передача данных через адрес
+    return $id;
+});*/
+
+/*Route::group(array('prefix'=>'users'), function () {
+	Route::get('/{id}', function ($id){
+    return $id;
+	});
+});*/
+
+Route::get('test/{id}', 'TestController@index');
+Route::get('photo-test', 'Photos\TestController@index');
+Route::get('home', 'HomeController@index');
+Auth::routes();
+
