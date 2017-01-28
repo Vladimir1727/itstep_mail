520bb7fb9f5c0efafda4a4949275070e47578aa7
Vladimir1727 <dvn125@gmail.com>
2017-01-28 13:07:35 +0200

    add Subscriber

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function subscribers(){//добавляет связь
+        return $this->hasMany('itstep\Models\Subscriber');//создаёт связь "один ко многим"
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,11 @@
 	});
 });*/
 
-Route::get('test/{id}', 'TestController@index');
-Route::get('photo-test', 'Photos\TestController@index');
 Route::get('home', 'HomeController@index');
 Route::get('/', 'HomeController@index');
 Auth::routes();
 Route::get('logout', 'HomeController@logout');
-
+Route::get('/model', 'HomeController@model');
+Route::group(['middleware'=>'auth'],function(){
+	Route::resource('subscribers', 'SubscriberController');//добавляет группы роутеров для SubscriberController
+});
