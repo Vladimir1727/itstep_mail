ad87cbb988176f1af55b87e3e79221f8e6bd0ab5
vladimir <dvn125@gmail.com>
2017-02-18 07:25:44 +0200

    add settings to DB
    seed email_send_types to DB
    store email_settings to DB

--- app/Http/Controllers/SendController.php
+++ app/Http/Controllers/SendController.php
@@ -4,6 +4,7 @@ namespace itstep\Http\Controllers;
 
 use Illuminate\Http\Request;
 use itstep\Mail\Test as TestMail;
+use itstep\Models\EmailSendSettingsModel as Settings;
 
 class SendController extends Controller
 {
@@ -33,10 +34,23 @@ class SendController extends Controller
     }
 
     public function showsettings(){
-        return view('send.settings');
+        $types=\DB::table('email_send_types')->get();
+        return view('send.settings',['types'=>$types]);
     }
 
     public function setsettings(Request $request){
-        echo "settings=".$request->type;
+        $is_set=Settings::where('user_id',\Auth::id())->first();
+        if (!$is_set){//нет настроек
+            $set=new Settings;
+            $set->type_send_id=$request->type;
+            $set->user_id=\Auth::id();
+            $set->save();
+        }
+        else{//есть настройки
+            $set=$is_set;
+            $set->type_send_id=$request->type;
+            $set->save();
+        }
+        return redirect()->back();
     }
 }
